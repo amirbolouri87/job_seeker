f0a43bee59316fb6cd1c955a9c079bcf1724ed5e
amir <amir.bolury66@gmail.com>
2024-01-23 09:19:38 -0800

    fix date time in get data from arbeitnow

--- scrap_arbeitnow.py
+++ scrap_arbeitnow.py
@@ -5,33 +5,26 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 
-# Replace 'your_driver_path' with the actual path to your webdriver executable
-# For example, if you are using Chrome, you need to download chromedriver: https://sites.google.com/chromium.org/driver/
 driver = webdriver.Firefox()
 
-# Replace 'your_url' with the actual URL where your HTML is located
 driver.get('https://www.arbeitnow.com')
 username = driver.find_element(By.XPATH, "//input[@id='search_jobs_desktop']")
 username.send_keys('django')
 driver.find_element(By.XPATH, "//select[@name='search_sort_by']/option[text()='Newest']").click()
-# Define the XPath to select all <li> elements within the <ul> element
 xpath_expression = "//ul[@id='results']/li"
 advertisements = []
 
-time.sleep(3) #sleep for 1 sec
+time.sleep(3)
 
-# Use find_elements_by_xpath to get a list of all matching elements
 list_items = driver.find_elements(By.XPATH, xpath_expression)
 
-# Iterate through the list_items and extract relevant information
 for item in list_items:
-    # Example: Extracting the href attribute from the first <a> element within each <li>
     link_element = item.find_element(By.XPATH, ".//a")
     link_url = link_element.get_attribute("href")
     link_text = link_element.get_attribute("aria-label")
     city = item.find_element(By.XPATH, ".//div[contains(@class, 'self-center')]/p")
     advertisment_datetime = item.find_element(By.XPATH, ".//div[contains(@class, 'self-center')]//time")
-    advertisement_datetime = advertisment_datetime.text
+    advertisement_datetime = advertisment_datetime.get_attribute("datetime")
     advertisements.append({
         "city": city.text,
         "advertise_url": link_url,
@@ -42,10 +35,13 @@ for item in list_items:
 for job_position in advertisements:
     driver.get(job_position['advertise_url'])
     print(job_position['advertise_url'])
+    print(job_position['city'])
+    print(job_position['advertisement_datetime'])
     time.sleep(3)
     advertise_title = driver.find_element(By.XPATH, "//div[@itemprop='description']")
     print(advertise_title.text)
     print('================================================================')
+    break
     # driver.close()
 
 
